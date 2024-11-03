02f0cd750c3699a1f54ddef87da7700542d2255e
Stinger-22 <maxym.sobol@gmail.com>
2024-11-03 17:16:21 +0100

    UI tests are performed on both Chrome and Firefox

--- tests/ui/conftest.py
+++ tests/ui/conftest.py
@@ -2,6 +2,7 @@ import logging
 
 import pytest
 from selenium import webdriver
+from selenium.webdriver.remote.webdriver import WebDriver
 
 from tests.ui.pages.log_in import LogInPage
 from tests.ui.pages.sign_up import SignUpPage
@@ -9,17 +10,21 @@ from tests.ui.pages.sign_up import SignUpPage
 LOGGER = logging.getLogger(__name__)
 
 
-@pytest.fixture
-def browser_chrome():
-    driver = webdriver.Chrome()
-    driver.implicitly_wait(5)
-    yield driver
-    driver.quit()
+@pytest.fixture(params = ["Chrome", "Firefox"])
+def driver(request):
+    _driver = None
+    if request.param == "Chrome":
+        _driver = webdriver.Chrome()
+    elif request.param == "Firefox":
+        _driver = webdriver.Firefox()
+    _driver.implicitly_wait(5)
+    yield _driver
+    _driver.quit()
 
 @pytest.fixture
-def log_in_page(browser_chrome):
-    return LogInPage(browser_chrome)
+def log_in_page(driver: WebDriver):
+    return LogInPage(driver)
 
 @pytest.fixture
-def sign_up_page(browser_chrome):
-    return SignUpPage(browser_chrome)
+def sign_up_page(driver: WebDriver):
+    return SignUpPage(driver)
